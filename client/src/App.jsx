2e92c74f311a422998f5e80248dcc0b867d2a53f
Eulojio <estudilloangel83@gmail.com>
2025-05-21 17:20:37 -0400

    Setting up app, I imported axios and react. I will be declaring the form inputsand I will also fetch dates.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
-  const [count, setCount] = useState(0)
+  //State for form inputs
+  const [form, setForm] = useState({ date: "", time: "", location: "", note: "" });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  //state for the list of date entries fetched from backend
+  const [dates, setDates] = useState([]);
+
+  //function to fetch all date entries from backend
+  const fetchDates = async () => {
+    try {
+      const res = await axios.get("/api/dates");
+      setDates(res.data);
+    } catch (err) {
+      alert("Failed to fetch dates");
+    }
+  };
+
 }
-
-export default App
