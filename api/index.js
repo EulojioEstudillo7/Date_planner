152f4efc9ffae7824a11c6e453b9842c0fbfec53
Eulojio <estudilloangel83@gmail.com>
2025-05-22 10:06:20 -0400

    This route will get all the dates and will also handle errors

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const DateModel = require("./models/Date");
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// This is the Middleware 
+app.use(cors());
+app.use(express.json());
+
+// Here I am connecting to mongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+
+//this will get all the dates
+app.get("/api/dates", async (req, res) => {
+    try {
+      const dates = await DateModel.find().sort({ date: 1 });
+      res.json(dates);
+    } catch (err) {
+      res.status(500).json({ message: "Server error" });
+    }
+  });
+
